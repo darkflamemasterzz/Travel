2f6bf75e1d561c902d60ed31273b966e6640a9d4
Bin <m13678954738@gmail.com>
2020-04-04 11:44:19 +0800

    初步完成轮播图

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,18 @@
 <template>
-  <HomeHeader></HomeHeader>
+  <div>
+    <HomeHeader></HomeHeader>
+    <HomeSwiper></HomeSwiper>
+  </div>
 </template>
 
 <script>
 import HomeHeader from './header'
+import HomeSwiper from './swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader: HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/components/home/swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+
+  data(){
+    return {
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination',
+            loop: true
+          },
+        },
+
+        swiperList:[
+              {
+                  id: '0001',
+                  imgUrl: "https://www.jetsetter.com/uploads/sites/7/2018/04/Yq6ObbTP-1380x690.jpeg"
+              },
+              {
+                  id: '0002',
+                  imgUrl: "https://www.pcma.org/wp-content/uploads/2019/08/Travel-tips.png"
+              }
+          ]
+    }
+  },
+
+  computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
+    },
+
+    mounted() {
+      console.log('Current Swiper instance object', this.swiper)
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper{
+    overflow: hidden 
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swiper-img{
+        width: 100%
+    }
+}
+</style>
