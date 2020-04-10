c8544e3e4d236962a7342d151d55b70eba7b78b5
Bin <m13678954738@gmail.com>
2020-04-10 11:31:09 +0800

    页面动态数据渲染

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -2,12 +2,13 @@
   <div>
       <CityHeader></CityHeader>
       <CitySearch></CitySearch>
-      <CityList></CityList>
-      <CityAlphabet></CityAlphabet>
+      <CityList :cities="cities" :hot="hotCities"></CityList>
+      <CityAlphabet :cities="cities"></CityAlphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './header'
 import CitySearch from './search'
 import CityList from './list'
@@ -19,6 +20,29 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data () {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo () {
+            axios.get('/static/mock/city.json')
+                .then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc (res) {
+            res = res.data
+            if (res.ret && res.data){
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    mounted () {
+        this.getCityInfo()
     }
 }
 </script>
