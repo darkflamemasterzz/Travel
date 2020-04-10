a9879c6e24cfdbb737a1c374d3f1617cff947e83
Bin <m13678954738@gmail.com>
2020-04-10 16:55:08 +0800

    完成city组件中字母表子组件和列表子组件的关联互动（兄弟组件间的联动）

--- src/components/city/alphabet.vue
+++ src/components/city/alphabet.vue
@@ -1,7 +1,16 @@
 <template>
   <ul class="list">
-      <li class="item" v-for="(item, key) of cities" :key="key">
-          {{key}}
+      <li 
+        class="item" 
+        v-for="item of letters" 
+        :key="item"
+        :ref="item"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        @click="handleLetterClick"
+      >
+          {{item}}
       </li>
   </ul>
 </template>
@@ -11,6 +20,42 @@ export default {
     name: 'CityAlphabet',
     props: {
         cities: Object
+    },
+    data () {
+        return {
+            touchStatus: false
+        }
+    },
+    computed: {
+        letters () {
+            const letters = []
+            for (let i in this.cities) {
+                letters.push(i)
+            }
+            return letters
+        }
+    },
+    methods: {
+        handleLetterClick (e) {
+            this.$emit('change', e.target.innerText)
+        },
+        handleTouchStart () {
+            this.touchStatus = true
+        },
+        handleTouchMove (e) {
+            if (this.touchStatus) {
+                const startY = this.$refs['A'][0].offsetTop
+                const touchY = e.touches[0].clientY - 79
+                const index = Math.floor((touchY - startY) / 20)
+                console.log(index)
+                if (index>=0 && index<this.letters.length){
+                    this.$emit('change', this.letters[index])
+                }
+            }
+        },
+        handleTouchEnd () {
+            this.touchStatus = false
+        }
     }
 }
 </script>
