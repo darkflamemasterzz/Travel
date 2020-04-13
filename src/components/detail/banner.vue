0bad9e86c5749f0e7e579abfc77a1c2c718d2e21
Bin <m13678954738@gmail.com>
2020-04-13 11:44:43 +0800

    完成详情页的banner和gallary轮播图

--- src/components/detail/banner.vue
+++ src/components/detail/banner.vue
@@ -1,16 +1,45 @@
 <template>
-    <div class="bannar">
-        <img class="bannar-img" src="https://imgs.qunarzz.com/p/tts7/1809/5f/e3032545a9ae0202.jpg_r_390x260x95_16346325.jpg"/>
-        <div class="bannar-info">
-            <div class="bannar-title">大夫山深林公园</div>
-            <div class="bannar-number">39</div>
+    <div>
+        <div class="bannar" @click="handleBannarClick">
+            <img class="bannar-img" src="https://imgs.qunarzz.com/p/tts7/1809/5f/e3032545a9ae0202.jpg_r_390x260x95_16346325.jpg"/>
+            <div class="bannar-info">
+                <div class="bannar-title">大夫山深林公园</div>
+                <div class="bannar-number">39</div>
+            </div>
         </div>
+        <common-gallary 
+            :imgs="imgs" 
+            v-show="showGallary"
+            @close="handleGallaryClose"
+        ></common-gallary>
     </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-    name: 'DetailBanner'
+    name: 'DetailBanner',
+    components: {
+        CommonGallary
+    },
+    data () {
+        return {
+
+            showGallary: false,
+            imgs: [
+                '//imgs.qunarzz.com/p/tts0/1609/96/32fe674b30c87202.jpg_r_480x320x95_a8494fdf.jpg',
+                '//imgs.qunarzz.com/p/p66/201304/17/384810d022dd28f793835fbb.jpg_256x160_6a7ec251.jpg'
+            ]
+        }
+    },
+    methods: {
+        handleBannarClick () {
+            this.showGallary = true
+        },
+        handleGallaryClose () {
+            this.showGallary = false
+        }
+    }
 }
 </script>
 
