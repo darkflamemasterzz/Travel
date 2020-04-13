246e251779e3c12aceee97ae47979013f9540ff9
Bin <m13678954738@gmail.com>
2020-04-13 16:04:20 +0800

    初步完成detail页的header、header和返回按钮的渐隐渐显效果

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <DetailBannar></DetailBannar>
+        <DetailHeader></DetailHeader>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBannar from './banner'
+import DetailHeader from './header'
 export default {
     name: 'Detail',
     components: {
-        DetailBannar
+        DetailBannar,
+        DetailHeader
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content{
+    height: 50rem
+    background: #eee
+}
 </style>
