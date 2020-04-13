128ca0f922126cbe6aaeff0178d9e1f026fc013b
Bin <m13678954738@gmail.com>
2020-04-13 09:54:32 +0800

    为详情页使用了动态路由、初步完成详情页动态路由的Bannar布局

---
+++ src/components/detail/banner.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bannar">
+        <img class="bannar-img" src="https://imgs.qunarzz.com/p/tts7/1809/5f/e3032545a9ae0202.jpg_r_390x260x95_16346325.jpg"/>
+        <div class="bannar-info">
+            <div class="bannar-title">大夫山深林公园</div>
+            <div class="bannar-number">39</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.bannar{
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .bannar-img{
+        width: 100%
+    }
+    .bannar-info{
+        display: flex
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        line-height: .6rem
+        color: #fff
+        background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+        .bannar-title{
+            font-size: .32rem
+            flex: 1
+            padding: 0 .2rem
+        }
+        .bannar-number{
+            padding: 0 .4rem
+            height: .32rem
+            line-height: .32rem
+            border-radius: .2rem
+            font-size: .24rem
+            background: rgba(0, 0, 0, .8)
+        }
+    }
+}
+</style>
---
+++ src/components/detail/detail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <DetailBannar></DetailBannar>
+    </div>
+</template>
+
+<script>
+import DetailBannar from './banner'
+export default {
+    name: 'Detail',
+    components: {
+        DetailBannar
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
