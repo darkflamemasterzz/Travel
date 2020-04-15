6088e00182b901370d0e14b2c0ea3be919c2d33f
Bin <m13678954738@gmail.com>
2020-04-15 09:44:42 +0800

    递归组件

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -2,6 +2,7 @@
     <div>
         <DetailBannar></DetailBannar>
         <DetailHeader></DetailHeader>
+        <DetailList :list="list"></DetailList>
         <div class="content"></div>
     </div>
 </template>
@@ -9,11 +10,31 @@
 <script>
 import DetailBannar from './banner'
 import DetailHeader from './header'
+import DetailList from './list'
 export default {
     name: 'Detail',
     components: {
         DetailBannar,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data () {
+        return {
+            list: [{
+                title: '成人票',
+                children: [{
+                    title: '成人三馆连票'
+                },{
+                    title: '成人五馆连票'
+                }]
+            },{
+                title: '学生票'
+            },{
+                title: '儿童票'
+            },{
+                title: '特惠票'
+            }]
+        }
     }
 }
 </script>
---
+++ src/components/detail/list.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="item" v-for="(item, index) of list" :key="index">
+            <div class="item-title">
+                <span class="item-title-icon"></span>
+                {{item.title}}
+            </div>
+            <div v-if="item.children" class="item-children">
+                <DetailList :list="item.children"></DetailList>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DetailList',
+    props: {
+        list: Array
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item-title{
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    border-bottom: 1px solid #eee
+
+}
+.item-children{
+    padding: 0 .2rem
+}
+</style>
